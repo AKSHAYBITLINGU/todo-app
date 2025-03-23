58d66327aef9986b7368bc18d125d5096241c9a3
AKSHAYBITLINGU <akshaybitlingu786@gmail.com>
2025-03-23 23:28:10 +0530

    Fix list_tasks to provide a friendlier empty list message

--- todo.py
+++ todo.py
@@ -5,7 +5,7 @@ def add_task(task):
     print(f'Task "{task}" added.')
 def list_tasks():
     if not tasks:
-        print("No tasks available.")
+        print("Your to-do list is empty. Add some tasks!")
     else:
         for idx, task in enumerate(tasks, start=1):
             print(f'{idx}. {task}')
